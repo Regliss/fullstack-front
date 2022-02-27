b5e985861e03e162f1076f90a0ae46175b818bef
darkregliss <pomart.thomas1@gmail.com>
2022-02-27 17:09:24 +0100

    one

--- src/pages/genreAdmin/index.js
+++ src/pages/genreAdmin/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useContext} from 'react';
+import React, {useState, useEffect} from 'react';
 import Titlepage from "../../components/UI/Title/TitlePage";
 import styles from "./index.module.scss";
 import genreService from '../../services/genre.service';
